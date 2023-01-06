5a46857d4aae640b22e39ff80f18cfdaa216fc27
morning2Jacob <106035061+morning2Jacob@users.noreply.github.com>
2023-01-06 18:13:14 +0800

    Add files via upload

--- WebCrawling_and_Visualization_RealEstate.py
+++ WebCrawling_and_Visualization_RealEstate.py
@@ -36,7 +36,7 @@ def web_one_page(url_p, header, ret_data):
     return ret_data
 
 #---------------------------------------------------------------------------------------------
-#homework1
+#Web Crawling
 url = 'https://rent.houseprice.tw/ws/list/%E5%8F%B0%E4%B8%AD%E5%B8%82_city/%E8%A5%BF%E5%B1%AF%E5%8D%80_zip/'
 header={
   'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER'
@@ -51,7 +51,7 @@ df.columns.unique()
 dataset = df.iloc[:, [2, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 18, 19, 22, 23, 27, 37]]
 #dataset.to_csv('D0790192_rent.csv', index=False)
 
-#homework2
+#台中市西屯區套房租金價格分布圖
 studio = dataset[dataset['rentPurPoseName'].isin(['獨立套房', '分租套房'])]
 studio_price = studio['rentPrice']
 plt.figure(figsize=(12, 6)).suptitle('台中市西屯區套房租金價格分布圖', fontsize=20)
@@ -60,10 +60,64 @@ plt.ticklabel_format(style='plain')
 plt.xticks(np.arange(0, studio_price.max(), 5000))
 plt.show()
 
-#homework3
+#台中市西屯區各類型物件的平均價格分布圖
 rentPurPoseName = dataset.groupby('rentPurPoseName')['rentPrice'].mean()
 rentPurPoseName_ind = rentPurPoseName.reset_index()
-plt.figure(figsize=(14, 6)).suptitle('台中市西屯區各類型物件的平均價格', fontsize=20)
+plt.figure(figsize=(14, 6)).suptitle('台中市西屯區各類型物件的平均價格分布圖', fontsize=20)
 plt.rcParams["font.family"]=["Microsoft JhengHei"] 
 plt.bar(rentPurPoseName_ind['rentPurPoseName'], rentPurPoseName_ind['rentPrice'])
 plt.show()
+
+#Dashboard
+from dash import Dash, dcc, html, Output, Input
+import plotly.express as px
+import plotly.graph_objects as go
+
+app = Dash(__name__)
+
+dataset = pd.read_csv('C:\\Users\\Jacob Kai\\D0790192_rent.csv')
+dataset_cp = dataset.copy()
+
+app.layout = html.Div([
+    
+    html.H1("Web Application Dashboards with Dash", style={'text-align' : 'center'}),
+    
+    dcc.Dropdown(['台中市西屯區套房租金價格分布圖', '台中市西屯區各類型物件的平均價格分布圖'],
+                 '台中市西屯區套房租金價格分布圖',
+                 id='slct_data'),
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+    
+    dcc.Graph(id='data_plot', figure={})
+
+])
+
+@app.callback(
+    Output('output_container', 'children'),
+    Output('data_plot', 'figure'),
+    Input('slct_data', 'value')
+)
+
+def update_graph(slct_option):
+    
+    container = "The option you've selected was {}".format(slct_option)
+    
+    if (slct_option == '台中市西屯區套房租金價格分布圖'):
+        studio_cp = dataset_cp[dataset_cp['rentPurPoseName'].isin(['獨立套房', '分租套房'])]
+        studio_price_cp = studio_cp['rentPrice']
+        fig = px.histogram(studio_price_cp, x='rentPrice', nbins=10)
+        
+        
+    if (slct_option == '台中市西屯區各類型物件的平均價格分布圖'):
+        rentPurPoseName_cp = dataset_cp.groupby('rentPurPoseName')['rentPrice'].mean()
+        rentPurPoseName_ind_cp = rentPurPoseName_cp.reset_index()
+        fig = px.bar(rentPurPoseName_ind_cp, x='rentPurPoseName', y='rentPrice')
+
+        
+    return container, fig
+
+        
+if __name__ == '__main__':
+    app.run_server(port=8030)
+        
+        
